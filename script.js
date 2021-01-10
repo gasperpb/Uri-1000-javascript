08676ada16e98ee1a3c6ae4f1de857e5a7c22ed8
gasperpb <achillespb@hotmail.com>
2021-01-09 23:39:45 -0300

    First commint

---
+++ script.js
@@ -0,0 +1,10 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+console.log("Hello World!")
+/**
+ * 1000
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
